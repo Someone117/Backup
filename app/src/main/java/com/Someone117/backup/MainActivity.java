31ebb47c379586b3c131d20eb5a6d28eb4dd292d
Someone117 <81842426+Someone117@users.noreply.github.com>
2023-09-20 18:07:27 -0700

    Added options to enter information

--- app/src/main/java/com/Someone117/backup/MainActivity.java
+++ app/src/main/java/com/Someone117/backup/MainActivity.java
@@ -3,21 +3,37 @@ package com.Someone117.backup;
 import android.Manifest;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.core.content.ContextCompat;
 import com.example.backup.R;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class MainActivity extends AppCompatActivity {
-    private static boolean done = true;
     public static final String CHANNEL_ID = "hi";
+    public final NetworkTask networkTask = new NetworkTask(this);
+
+    @RequiresApi(api = Build.VERSION_CODES.Q)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SharedPreferences preferences = getPreferences(MODE_PRIVATE);
 
         CharSequence name = "My Channel";
         String description = "My Channel Description";
@@ -34,15 +50,95 @@ public class MainActivity extends AppCompatActivity {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_BACKGROUND_LOCATION}, MY_PERMISSIONS_REQUEST_BACKGROUND_LOCATION);
         }
 
-        Button button = findViewById(R.id.sync);
-        button.setOnClickListener(v -> {
-            new NetworkTask(this).transferFiles();
+
+        SharedPreferences.Editor editor = preferences.edit();
+
+        TextView location = findViewById(R.id.disk);
+        location.setText(preferences.getString("disk", "NetworkLocation"));
+        location.setOnFocusChangeListener((v, hasFocus) -> {
+            editor.putString("disk", location.getText().toString());
+            editor.apply();
         });
 
+        TextView ssid = findViewById(R.id.ssid);
+        ssid.setText(preferences.getString("ssid", "Network SSID"));
+        ssid.setOnFocusChangeListener((v, hasFocus) -> {
+            editor.putString("ssid", ssid.getText().toString());
+            editor.apply();
 
-        if(done) { // do not reschedule alarm
-            BackupScheduler.scheduleBackup(this);
-            done = false;
+        });
+
+        TextView ip = findViewById(R.id.ipAddress);
+        ip.setText(preferences.getString("ip", "IP Address"));
+        ip.setOnFocusChangeListener((v, hasFocus) -> {
+            editor.putString("ip", ip.getText().toString());
+            editor.apply();
+        });
+
+        TextView userName = findViewById(R.id.sshUsername);
+        userName.setText(preferences.getString("userName", "SSH Username"));
+        userName.setOnFocusChangeListener((v, hasFocus) -> {
+            editor.putString("userName", userName.getText().toString());
+            editor.apply();
+        });
+
+        TextView pw = findViewById(R.id.password);
+        pw.setText(preferences.getString("pw", ""));
+        pw.setOnFocusChangeListener((v, hasFocus) -> {
+            editor.putString("pw", pw.getText().toString());
+            editor.apply();
+        });
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(v -> {
+            Record r = new Record(userName.getText().toString().trim(), pw.getText().toString().trim(), ip.getText().toString().trim(), ssid.getText().toString().trim(), location.getText().toString().trim());
+            backup(r);
+        });
+    }
+
+    public void backup(Record record) {
+        // get ssid of network
+        WifiManager wifiManager = (WifiManager) this.getSystemService(Context.WIFI_SERVICE);
+        WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+        String ssid = wifiInfo.getSSID();
+
+        // check if the phone is on the right network
+        if (ssid.equals("\"" + record.NetworkSSID + "\"")) {
+            ExecutorService executor;
+
+            executor = Executors.newFixedThreadPool(1);
+
+            // Create and submit a network task for execution
+            networkTask.setRecord(record);
+            executor.submit(networkTask);
+        } else {
+            // notify the user that they are not on the correct Wi-Fi network
+            NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                    .setSmallIcon(R.drawable.ic_launcher_background)
+                    .setContentTitle("Incorrect Wifi")
+                    .setContentText(ssid)
+                    .setPriority(NotificationCompat.PRIORITY_MIN);
+
+
+            notificationManager.notify(0, builder.build());
+        }
+    }
+
+    public static class Record {
+        public String username;
+        public String password;
+        public String ipAddress;
+        public String NetworkSSID;
+
+        public String location;
+
+        public Record(String username, String password, String ipAddress, String networkSSID, String location) {
+            this.username = username;
+            this.password = password;
+            this.ipAddress = ipAddress;
+            this.NetworkSSID = networkSSID;
+            this.location = location;
         }
     }
 
--- app/src/main/java/com/Someone117/backup/NetworkTask.java
+++ app/src/main/java/com/Someone117/backup/NetworkTask.java
@@ -8,7 +8,12 @@ import com.example.backup.R;
 import com.jcraft.jsch.*;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.text.CharacterIterator;
+import java.text.StringCharacterIterator;
 import java.util.ArrayList;
+import java.util.Locale;
 import java.util.Objects;
 import java.util.Vector;
 
@@ -16,110 +21,17 @@ import static com.Someone117.backup.MainActivity.CHANNEL_ID;
 
 public class NetworkTask implements Runnable {
     private final Context id;
-    NetworkTask(Context a) {
-        this.id = a;
-    }
-    @Override
-    public void run() {
-        transferFiles();
-    }
-    public void transferFiles() {
-        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(id);
 
-        // connect to ssh server
-        //ToDo: fill in your information
-        JSch jsch = new JSch();
-        String host = "host.location";
-        String username = "username";
-        String password = "password";
-        java.util.Properties config = new java.util.Properties();
-        config.put("StrictHostKeyChecking", "no");
-        //ToDo: end fill in your information
+    private MainActivity.Record record;
 
-        File rootsd = Environment.getExternalStorageDirectory();
-        //ToDo fill in your information
-        File local = new File(rootsd.getAbsolutePath() + "/local/file/location");
-        String remoteFolderPath = "/remote/folder";
-        //ToDo: end fill in your information
-        ArrayList<File> fileList = getFiles(local);
-
-        try{
-            Session session = jsch.getSession(username, host, 22);
-            // use
-            session.setPassword(password);
-            session.setConfig(config);
-            session.connect();
-
-            ChannelSftp channel = (ChannelSftp) session.openChannel("sftp");
-            channel.connect();
-
-
-            // remove files that were already uploaded
-            Vector<ChannelSftp.LsEntry> compare = channel.ls(remoteFolderPath);
-            removeDuplicates(fileList, compare);
-
-            // Send file
-            if(fileList.size() > 0) {
-                String[] files = new String[fileList.size()];
-                for(int i = 0; i < fileList.size(); i++) {
-                    files[i] = fileList.get(i).getAbsolutePath();
-                }
-                // notify the user that files are uploading
-                NotificationCompat.Builder builder = new NotificationCompat.Builder(id, CHANNEL_ID)
-                        .setSmallIcon(R.drawable.ic_launcher_background)
-                        .setContentTitle("Uploading files")
-                        .setContentText(fileList.size() + " files to upload")
-                        .setPriority(NotificationCompat.PRIORITY_DEFAULT);
-
-                int PROGRESS_MAX = files.length;
-                int PROGRESS_CURRENT = 0;
-                builder.setProgress(PROGRESS_MAX, PROGRESS_CURRENT, false);
-                notificationManager.notify(1, builder.build());
-
-                // upload the files
-                for(int i = 0; i < files.length; i++) {
-                    channel.put(files[i], remoteFolderPath, ChannelSftp.APPEND);
-                    PROGRESS_CURRENT = i;
-                    builder.setProgress(PROGRESS_MAX, PROGRESS_CURRENT, false)
-                            .setContentText(i + " files done out of " + fileList.size() + " files");
-
-                    notificationManager.notify(1, builder.build());
-                }
-                builder.setContentText("Upload Complete")
-                        .setProgress(0,0,false);
-                notificationManager.notify(1, builder.build());
-            } else {
-                NotificationCompat.Builder builder = new NotificationCompat.Builder(id, CHANNEL_ID)
-                        .setSmallIcon(R.drawable.ic_launcher_background)
-                        .setContentTitle("No files to upload")
-                        .setContentText("")
-                        .setPriority(NotificationCompat.PRIORITY_MIN);
-
-                notificationManager.notify(1, builder.build());
-            }
-
-            // Disconnect and close the SSH session and channel
-            channel.disconnect();
-            session.disconnect();
-        } catch (SftpException | JSchException e) {
-            e.printStackTrace();
-            NotificationCompat.Builder builder = new NotificationCompat.Builder(this.id, CHANNEL_ID)
-                    .setSmallIcon(R.drawable.ic_launcher_background)
-                    .setContentTitle("Error!")
-                    .setContentText(e.getLocalizedMessage())
-                    .setPriority(NotificationCompat.PRIORITY_DEFAULT);
-
-            // notificationId is a unique int for each notification that you must define
-            notificationManager.notify(3, builder.build());
-
-        }
-
+    NetworkTask(Context id) {
+        this.id = id;
     }
 
     // Remove files that were already uploaded
     private static void removeDuplicates(ArrayList<File> fileList, Vector<ChannelSftp.LsEntry> compare2) {
-        for(int i = fileList.size() - 1; i >= 0; i--) {
-            if(fileList.get(i).getName().startsWith(".trashed")) { //ToDo: some phones use this to denote trash, remove if you want
+        for (int i = fileList.size() - 1; i >= 0; i--) {
+            if (fileList.get(i).getName().startsWith(".trashed")) { //ToDo: some phones use this to denote trash, remove if you want
                 fileList.remove(i);
             } else {
                 boolean found = false;
@@ -145,5 +57,119 @@ public class NetworkTask implements Runnable {
             }
         }
         return files;
+    }
+
+    public static String humanReadableByteCountBin(long bytes) {
+        long absB = bytes == Long.MIN_VALUE ? Long.MAX_VALUE : Math.abs(bytes);
+        if (absB < 1024) {
+            return bytes + " B";
+        }
+        long value = absB;
+        CharacterIterator ci = new StringCharacterIterator("KMGTPE");
+        for (int i = 40; i >= 0 && absB > 0xfffccccccccccccL >> i; i -= 10) {
+            value >>= 10;
+            ci.next();
+        }
+        value *= Long.signum(bytes);
+        return String.format(Locale.getDefault(), "%.1f %ciB", value / 1024.0, ci.current());
+    }
+
+
+    @Override
+    public void run() {
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(id);
+
+        // connect to ssh server
+        JSch jsch = new JSch();
+        String host = record.ipAddress;
+        String username = record.username;
+        String password = record.password;
+        java.util.Properties config = new java.util.Properties();
+        config.put("StrictHostKeyChecking", "no");
+
+        File rootsd = Environment.getExternalStorageDirectory();
+        File local = new File(rootsd.getAbsolutePath() + "/DCIM/Camera");
+
+        String remoteFolderPath = record.location;
+        ArrayList<File> fileList = getFiles(local);
+
+        try {
+            Session session = jsch.getSession(username, host, 22);
+            // use
+            session.setPassword(password);
+            session.setConfig(config);
+            session.connect();
+
+            ChannelSftp channel = (ChannelSftp) session.openChannel("sftp");
+            channel.connect();
+
+
+            // remove files that were already uploaded
+            Vector<ChannelSftp.LsEntry> compare = channel.ls(remoteFolderPath);
+            removeDuplicates(fileList, compare);
+
+            // Send file
+            if (fileList.size() > 0) {
+                String[] files = new String[fileList.size()];
+                long size = 0;
+                for (int i = 0; i < fileList.size(); i++) {
+                    files[i] = fileList.get(i).getAbsolutePath();
+                    size += Files.size(fileList.get(i).toPath());
+                }
+                // notify the user that files are uploading
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(id, CHANNEL_ID)
+                        .setSmallIcon(R.drawable.ic_launcher_background)
+                        .setContentTitle("Uploading files: " + humanReadableByteCountBin(size))
+                        .setContentText(fileList.size() + " files to upload")
+                        .setPriority(NotificationCompat.PRIORITY_MIN);
+
+                int PROGRESS_MAX = files.length;
+                int PROGRESS_CURRENT = 0;
+                builder.setProgress(PROGRESS_MAX, PROGRESS_CURRENT, false);
+                notificationManager.notify(1, builder.build());
+
+                // upload the files
+                for (int i = 0; i < files.length; i++) {
+                    channel.put(files[i], remoteFolderPath, ChannelSftp.APPEND);
+                    PROGRESS_CURRENT = i;
+                    builder.setProgress(PROGRESS_MAX, PROGRESS_CURRENT, false)
+                            .setContentText(i + " files done out of " + fileList.size() + " files")
+                            .setPriority(NotificationCompat.PRIORITY_MIN);
+
+                    notificationManager.notify(1, builder.build());
+                }
+                builder.setContentText("Upload Complete")
+                        .setProgress(0, 0, false);
+                notificationManager.notify(1, builder.build());
+            } else {
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(id, CHANNEL_ID)
+                        .setSmallIcon(R.drawable.ic_launcher_background)
+                        .setContentTitle("No files to upload")
+                        .setContentText("")
+                        .setPriority(NotificationCompat.PRIORITY_MIN);
+
+                notificationManager.notify(1, builder.build());
+            }
+
+//             Disconnect and close the SSH session and channel
+            channel.disconnect();
+            session.disconnect();
+        } catch (SftpException | JSchException | IOException e) {
+            e.printStackTrace();
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(this.id, CHANNEL_ID)
+                    .setSmallIcon(R.drawable.ic_launcher_background)
+                    .setContentTitle("Error!")
+                    .setContentText(e.getLocalizedMessage())
+                    .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+            // notificationId is a unique int for each notification that you must define
+            notificationManager.notify(3, builder.build());
+
+        }
+
+    }
+
+    public void setRecord(MainActivity.Record record) {
+        this.record = record;
     }
 }
